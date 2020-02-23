2d7d56f3810bd9e3be9af5c1225197a005172e0e
Jayesh Tembhekar <jtembhekar2@gmail.com>
2020-02-23 17:23:29 +0530

    Initial commit

---
+++ insert_many.js
@@ -0,0 +1,14 @@
+db.customers.insert([
+  {
+    first_name: "Sam",
+    last_name: "Smith"
+  },
+  {
+    first_name: "Michelle",
+    last_name: "Johnson"
+  },
+  {
+    first_name: "Mike",
+    last_name: "Williams"
+  }
+]);
